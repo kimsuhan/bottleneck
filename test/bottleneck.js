c85f9bc216e945ec4c34f9768ab62b210d9509fe
suhankim <k_yakima@naver.com>
2026-03-24 22:00:37 +0900

    omx(team): auto-checkpoint worker-1 [unknown]

--- test/bottleneck.js
+++ test/bottleneck.js
@@ -5,3 +5,9 @@ if (process.env.BUILD === 'es5') {
 } else {
   module.exports = require('../lib/index.js')
 }
+
+if (process.env.DATASTORE === 'redis' || process.env.DATASTORE === 'ioredis') {
+  var workerScope = (process.env.OMX_TEAM_WORKER || 'worker').replace(/[\\/]/g, '-')
+  var testScope = process.pid + '-' + workerScope
+  module.exports.prototype.instanceDefaults.id = '<no-id>-' + testScope
+}
